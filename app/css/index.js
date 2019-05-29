69a6e88bca2b59a6f18890237664a31ad44dda93
anderjs <andersonav37@gmail.com>
2019-05-29 18:31:11 -0400

    Color palette

--- app/css/index.js
+++ app/css/index.js
@@ -1,3 +1,10 @@
-module.exports = {
-
-};
+/**
+ * @prettier
+ */
+export const Colors = {
+  BLUE_DARK: '#2A5298',
+  BLUE_LIGHT: '#F4F6FA',
+  GRAY_MEDIUM: '#F5F5F5',
+  TEXT_LIGHT: '#787878',
+  TEXT_STANDARD: '#404040',
+}
