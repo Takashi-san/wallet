0265ab059fc1e6229f656aae2fd2ab6cb1f68890
Daniel Lugo <user.email>
2019-06-05 18:28:36 -0400

    chat screen in root stack navigator

--- app/factories/RootStack.js
+++ app/factories/RootStack.js
@@ -5,6 +5,7 @@ import { createStackNavigator } from 'react-navigation'
 
 import LoggedInScreenNavigator from './MainDrawerNavigator'
 
+import Chat from '../screens/Chat'
 import LoginScreen from '../screens/Login/Login'
 import ConnectToHost from '../screens/Login/connectToHost'
 import ConnectToWallet from '../screens/Login/connectToWallet'
@@ -15,8 +16,11 @@ import SearchForUsers from '../screens/Search/Search'
 import EditProfile from '../screens/Profile/EditProfile'
 import PublicProfile from '../screens/Profile/PublicProfile'
 
+export const CHAT_KEY = 'CHAT_KEY'
+
 export default createStackNavigator(
   {
+    [CHAT_KEY]: Chat,
     ConnectToHost: ConnectToHost,
     ConfirmPhrase: ConfirmPhrase,
     ConnectToWallet: ConnectToWallet,
