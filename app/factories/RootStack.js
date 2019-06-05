5666fc726f56c52d3d1fa01f879902c0dcd0dcce
Daniel Lugo <user.email>
2019-06-05 18:00:51 -0400

    refactor root stack

---
+++ app/factories/RootStack.js
@@ -0,0 +1,34 @@
+/**
+ * @prettier
+ */
+import { createStackNavigator } from 'react-navigation'
+
+import LoggedInScreenNavigator from './MainDrawerNavigator'
+
+import LoginScreen from '../screens/Login/Login'
+import ConnectToHost from '../screens/Login/connectToHost'
+import ConnectToWallet from '../screens/Login/connectToWallet'
+import NewWallet from '../screens/Login/newWallet'
+import ConfirmPhrase from '../screens/Login/confirmPhrase'
+import FundWallet from '../screens/Login/fundWallet'
+import SearchForUsers from '../screens/Search/Search'
+import EditProfile from '../screens/Profile/EditProfile'
+import PublicProfile from '../screens/Profile/PublicProfile'
+
+export default createStackNavigator(
+  {
+    ConnectToHost: ConnectToHost,
+    ConfirmPhrase: ConfirmPhrase,
+    ConnectToWallet: ConnectToWallet,
+    EditProfile: EditProfile,
+    PublicProfile: PublicProfile,
+    FundWallet: FundWallet,
+    Login: LoginScreen,
+    LoggedInScreenNavigator: LoggedInScreenNavigator,
+    NewWallet: NewWallet,
+    SearchForUsers: SearchForUsers,
+  },
+  {
+    initialRouteName: 'Login',
+  },
+)
