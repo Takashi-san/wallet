7b6678c17153b4caca62d6def77c6bcb458a48b8
Daniel Lugo <user.email>
2019-07-10 16:07:28 -0400

    jobs

--- app/screens/contact-api/index.js
+++ app/screens/contact-api/index.js
@@ -4,5 +4,6 @@
 import * as Actions from './actions'
 import * as Events from './events'
 import * as Schema from './schema'
+import { spinupJobs } from './jobs'
 
-export { Actions, Events, Schema }
+export { Actions, Events, Schema, spinupJobs }
