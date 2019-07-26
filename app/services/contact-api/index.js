3b797f43657bfa972751a60dbb9bd84ee14a88f2
Daniel Lugo <user.email>
2019-07-26 12:42:52 -0400

    export gun from contact api

--- app/services/contact-api/index.js
+++ app/services/contact-api/index.js
@@ -3,8 +3,9 @@
  */
 import * as Actions from './actions'
 import * as Events from './events'
+import * as Gun from './gun'
 import * as Schema from './schema'
 import * as Testing from './testing'
 import { spinupJobs } from './jobs'
 
-export { Actions, Events, Schema, spinupJobs, Testing }
+export { Actions, Events, Gun, Schema, spinupJobs, Testing }
