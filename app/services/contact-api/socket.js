44681409e265052de3e49e5c49885a5d89844b9f
Daniel Lugo <danlugo92@gmail.com>
2019-09-10 17:30:28 -0400

    new socket connection dynamics

--- app/services/contact-api/socket.js
+++ app/services/contact-api/socket.js
@@ -1,11 +1,7 @@
 /**
  * @format
  */
-
 import SocketIO from 'socket.io-client'
-
-// @ts-ignore
-const { SOCKET_PORT, SOCKET_URL } = require('../../../env.json')
 
 /**
  * @typedef {object} SimpleSocket
@@ -16,14 +12,59 @@ const { SOCKET_PORT, SOCKET_URL } = require('../../../env.json')
  */
 
 /**
- * @private
  * @type {SimpleSocket}
  */
-export const socket = SocketIO(`http://${SOCKET_URL}:${SOCKET_PORT}`, {
-  transports: ['websocket'],
-  jsonp: false,
-})
+export let socket
 
-export const connect = () => {
+export const disconnect = () => {
+  if (socket) {
+    // @ts-ignore
+    socket.disconnect()
+    // @ts-ignore
+    socket.off()
+  }
+}
+
+/**
+ * @param {string} url
+ * @returns {void}
+ */
+export const connect = url => {
+  disconnect()
+
+  socket = SocketIO(url, {
+    transports: ['websocket'],
+    jsonp: false,
+  })
+
+  socket.on('connect_error', e => {
+    console.warn(e.message)
+  })
+
+  socket.on('connect_error', error => {
+    console.warn(`connect_error: ${error}`)
+  })
+
+  socket.on('connect_timeout', timeout => {
+    console.warn(`connect_timeout: ${timeout}`)
+  })
+
+  socket.on('error', error => {
+    console.warn(`Socket.socket.on:error: ${error}`)
+  })
+
+  socket.on('reconnect_attempt', attemptNumber => {
+    console.warn(`Socket.socket.on:reconnect_attempt: ${attemptNumber}`)
+  })
+
+  socket.on('disconnect', reason => {
+    console.warn(`reason for disconnect: ${reason}`)
+
+    if (reason === 'io server disconnect') {
+      // https://Socket.socket.io/docs/client-api/#Event-%E2%80%98disconnect%E2%80%99
+      socket.connect()
+    }
+  })
+
   socket.connect()
 }
