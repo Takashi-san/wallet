eca95493a248700b6dac8f33ce936d1293192aff
Daniel Lugo <danlugo92@gmail.com>
2019-09-11 15:23:28 -0400

    new root stack

--- app/factories/RootStack.js
+++ app/factories/RootStack.js
@@ -1,13 +1,15 @@
 /**
  * @prettier
  */
-import { createStackNavigator } from 'react-navigation'
+
+import { createStackNavigator, createSwitchNavigator } from 'react-navigation'
+import debounce from 'lodash/debounce'
+import once from 'lodash/once'
 
 import LoggedInScreenNavigator from './MainDrawerNavigator'
 
 import Chat, { CHAT_ROUTE } from '../screens/Chat'
 import Chats, { CHATS_ROUTE } from '../screens/Chats'
-import LoginScreen from '../screens/Login/Login'
 import ConnectToHost from '../screens/Login/connectToHost'
 import ConnectToWallet from '../screens/Login/connectToWallet'
 import NewWallet from '../screens/Login/newWallet'
@@ -17,9 +19,24 @@ import SearchForUsers from '../screens/Search/Search'
 import EditProfile from '../screens/Profile/EditProfile'
 import PublicProfile from '../screens/Profile/PublicProfile'
 
-import FakeLogin from '../screens/FakeLogin'
+import ChooseDisplayName, {
+  CHOOSE_DISPLAY_NAME,
+} from '../screens/ChooseDisplayName'
+import Login, { LOGIN } from '../screens/Login'
+import Register, { REGISTER } from '../screens/Register'
+import User from '../screens/TheirProfile'
+import Users from '../screens/Users'
 
-export default createStackNavigator(
+import MyProfile from '../screens/MyProfile'
+import Loading, { LOADING } from '../screens/Loading'
+
+import * as ContactAPI from '../services/contact-api'
+import * as Cache from '../services/cache'
+import * as NavigationService from '../services/navigation'
+
+const APP = 'APP'
+
+const App = createStackNavigator(
   {
     [CHAT_ROUTE]: Chat,
     [CHATS_ROUTE]: Chats,
@@ -29,13 +46,79 @@ export default createStackNavigator(
     EditProfile: EditProfile,
     PublicProfile: PublicProfile,
     FundWallet: FundWallet,
-    Login: LoginScreen,
     LoggedInScreenNavigator: LoggedInScreenNavigator,
     NewWallet: NewWallet,
     SearchForUsers: SearchForUsers,
-    FakeLogin: FakeLogin,
+    User,
+    Users,
+    MyProfile,
   },
   {
-    initialRouteName: 'FakeLogin',
+    headerLayoutPreset: 'center',
+    initialRouteName: 'MyProfile',
   },
 )
+
+const AUTH = 'AUTH'
+
+const Auth = createStackNavigator(
+  {
+    [LOGIN]: Login,
+    [REGISTER]: Register,
+  },
+  {
+    initialRouteName: LOGIN,
+  },
+)
+
+const MainSwitch = createSwitchNavigator(
+  {
+    [AUTH]: Auth,
+    [APP]: App,
+    [LOADING]: Loading,
+    [CHOOSE_DISPLAY_NAME]: ChooseDisplayName,
+  },
+  {
+    initialRouteName: LOADING,
+  },
+)
+
+let nodeIP = ''
+
+export const setup = async () => {
+  try {
+    ContactAPI.Events.onAuth(ad => {
+      const currentRoute = NavigationService.getCurrentRoute()
+      // Anytime un-authentication happens immediately navigate to the login screen.
+
+      if (ad === null && currentRoute !== AUTH) {
+        NavigationService.navigate(AUTH)
+      }
+
+      // empty string handles case when we get auth data from cache
+      if (ad !== null && (currentRoute === LOGIN || currentRoute === '')) {
+        NavigationService.navigate(LOADING)
+
+        // Get the display name once after login.
+        // Make the user choose his/her display name if not already set.
+        ContactAPI.Events.onDisplayName(
+          debounce(
+            once(dn => {
+              if (dn === null) {
+                NavigationService.navigate(CHOOSE_DISPLAY_NAME)
+              } else {
+                NavigationService.navigate(APP)
+              }
+            }),
+          ),
+        )
+      }
+
+      Cache.writeStoredAuthData(ad)
+    })
+  } catch (e) {
+    console.warn(e)
+  }
+}
+
+export default MainSwitch
