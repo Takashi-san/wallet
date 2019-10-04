2cdaf0ffa3d47a53e2d2d7a9a9d5e4265163b0b1
Daniel Lugo <danlugo92@gmail.com>
2019-10-04 19:32:26 -0400

    add QR library

--- android/app/src/main/java/com/shockwallet/MainApplication.java
+++ android/app/src/main/java/com/shockwallet/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.BV.LinearGradient.LinearGradientPackage;
+import com.horcrux.svg.SvgPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new LinearGradientPackage(),
+            new SvgPackage(),
             new VectorIconsPackage()
       );
     }
