5b570a46df647764789e0e029728e96f54dfd7e2
Daniel Lugo <danlugo92@gmail.com>
2019-10-05 18:53:19 -0400

    link async-storage

--- android/app/src/main/java/com/shockwallet/MainApplication.java
+++ android/app/src/main/java/com/shockwallet/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.BV.LinearGradient.LinearGradientPackage;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.horcrux.svg.SvgPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new LinearGradientPackage(),
+            new AsyncStoragePackage(),
             new RNCameraPackage(),
             new SvgPackage(),
             new VectorIconsPackage()
