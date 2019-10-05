44c944da88af60f2d33c5e91780535f5f74ccbd1
Daniel Lugo <danlugo92@gmail.com>
2019-10-05 17:29:17 -0400

    add react-native-camera

--- android/app/src/main/java/com/shockwallet/MainApplication.java
+++ android/app/src/main/java/com/shockwallet/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.BV.LinearGradient.LinearGradientPackage;
+import org.reactnative.camera.RNCameraPackage;
 import com.horcrux.svg.SvgPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new LinearGradientPackage(),
+            new RNCameraPackage(),
             new SvgPackage(),
             new VectorIconsPackage()
       );
