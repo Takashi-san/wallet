9c058969aaa95504838769f7e2d3a398cd9859d4
emad-salah <emads14@gmail.com>
2019-10-08 18:57:26 +0100

    Added Fab menu for advanced screen

--- app/screens/Advanced/Accordion/index.js
+++ app/screens/Advanced/Accordion/index.js
@@ -1,10 +1,12 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { View, Text, StyleSheet, TouchableHighlight, ScrollView, TouchableOpacity } from "react-native";
 import LinearGradient from "react-native-linear-gradient";
+import Icon from "react-native-vector-icons/Entypo";
 
 export default class Accordion extends Component {
   state = {
-    open: false
+    open: false,
+    menuOpen: false
   };
 
   componentDidMount() {
@@ -12,6 +14,46 @@ export default class Accordion extends Component {
     this.setState({
       open
     });
+  }
+
+  toggleMenuOpen = () => {
+    const { menuOpen } = this.state;
+    this.setState({
+      menuOpen: !menuOpen
+    })
+  }
+
+  renderTRXMenu = () => {
+    const { title, open } = this.props;
+    const { menuOpen } = this.state;
+    const supportedAccordions = ['transactions'];
+    if (open && supportedAccordions.includes(title.toLowerCase())) {
+      return (
+        <View style={styles.accordionMenu}>
+          <View style={[styles.accordionMenuItem, {
+            opacity: menuOpen ? 1 : 0
+          }]}>
+            <View style={styles.accordionMenuItemIcon}>
+              <Icon name="link" size={20} color="#294f93" />
+            </View>
+            <Text style={styles.accordionMenuItemText}>Generate</Text>
+          </View>
+          <View style={[styles.accordionMenuItem, {
+            opacity: menuOpen ? 1 : 0
+          }]}>
+            <View style={styles.accordionMenuItemIcon}>
+              <Icon name="flash" size={20} color="#294f93" />
+            </View>
+            <Text style={styles.accordionMenuItemText}>Send</Text>
+          </View>
+          <TouchableOpacity style={styles.accordionMenuBtn} onPress={this.toggleMenuOpen}>
+            <Icon name="plus" size={30} color="white" />
+          </TouchableOpacity>
+        </View>
+      );
+    }
+
+    return null;
   }
 
   render() {
@@ -30,6 +72,7 @@ export default class Accordion extends Component {
         }}>
           {children}
         </ScrollView>
+        {this.renderTRXMenu()}
       </View>
     );
   }
@@ -50,5 +93,42 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "bold",
     color: "white"
+  },
+  accordionMenu: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    padding: 10
+  },
+  accordionMenuItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    paddingRight: 20,
+    borderRadius: 100,
+    backgroundColor: 'white',
+    marginRight: 15,
+    elevation: 3
+  },
+  accordionMenuItemIcon: {
+    marginRight: 10
+  },
+  accordionMenuItemText: {
+    color: "#294f93",
+    fontWeight: 'bold'
+  },
+  accordionMenuBtn: {
+    width: 45,
+    height: 45,
+    borderRadius: 100,
+    backgroundColor: "#f5a623",
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 3
   }
 });
