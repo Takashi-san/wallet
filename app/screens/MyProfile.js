8b0b00a4a16ab356910c1f04f7d4c52232ec4733
Daniel Lugo <danlugo92@gmail.com>
2019-10-14 15:14:58 -0400

    all new MyProfile

--- app/screens/MyProfile.js
+++ app/screens/MyProfile.js
@@ -1,59 +1,162 @@
 /** @format */
 import React from 'react'
 
-import { TextInput, Button, View, AsyncStorage } from 'react-native'
+import { Text, StyleSheet, ActivityIndicator } from 'react-native'
+import { View } from 'react-native'
+import EntypoIcons from 'react-native-vector-icons/Entypo'
 
 import * as API from '../services/contact-api'
-import { CHATS_ROUTE } from './Chats'
+import ShockAvatar from '../components/ShockAvatar'
+import ShockButton from '../components/ShockButton'
+import QR from './WalletOverview/QR'
+import Pad from '../components/Pad'
 
+export const MY_PROFILE = 'MY_PROFILE'
+
+/**
+ * @typedef {object} State
+ * @prop {API.Events.AuthData} authData
+ * @prop {string|null} displayName
+ * @prop {string|null} handshakeAddr
+ */
+
+/**
+ * @augments React.PureComponent<{}, State, never>
+ */
 export default class MyProfile extends React.PureComponent {
-  state = {
-    displayName: '',
-  }
-
   /**
-   * @param {string} displayName
-   * @returns {void}
+   * @type {import('react-navigation').NavigationBottomTabScreenOptions}
    */
-  onChange = displayName => {
-    this.setState({
-      displayName,
-    })
+  static navigationOptions = {
+    tabBarIcon: ({ tintColor }) => {
+      return ((
+        <EntypoIcons
+          color={tintColor === null ? undefined : tintColor}
+          name="user"
+          // reverseColor={'#CED0CE'}
+          size={22}
+        />
+      ))
+    },
   }
 
-  onPress = () => {
-    API.Actions.setDisplayName(this.state.displayName)
+  /** @type {State} */
+  state = {
+    authData: null,
+    displayName: null,
+    handshakeAddr: null,
   }
 
-  goToChats = () => {
-    this.props.navigation.navigate(CHATS_ROUTE)
+  onAuthDataUnsub = () => {}
+  onDisplayNameUnsub = () => {}
+  onHandshakeAddressUnsub = () => {}
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        authData: {
+          publicKey: 'pk',
+          token: 'token',
+        },
+        displayName: 'John',
+      })
+    }, 500)
+    {
+      // this.onAuthDataUnsub = API.Events.onAuth(ad => {
+      //   if (ad === null) {
+      //     return
+      //   }
+      //   this.setState({
+      //     authData: ad,
+      //   })
+      // })
+      // this.onDisplayNameUnsub = API.Events.onDisplayName(dn => {
+      //   this.setState({
+      //     displayName: dn,
+      //   })
+      // })
+      // this.onHandshakeAddressUnsub = API.Events.onHandshakeAddr(addr => {
+      //   this.setState({
+      //     handshakeAddr: addr,
+      //   })
+      // })
+    }
+  }
+
+  componentWillUnmount() {
+    this.onAuthDataUnsub()
+    this.onDisplayNameUnsub()
+    this.onHandshakeAddressUnsub()
   }
 
   genHandAddr = () => {
-    API.Actions.generateNewHandshakeNode()
-  }
+    setTimeout(() => {
+      this.setState({
+        handshakeAddr: 'jaksljdklasjfhaskjdhkj',
+      })
+    }, 100)
 
-  logout = () => {
-    API.Actions.logout()
-  }
-
-  clearAllCache = () => {
-    AsyncStorage.clear()
+    // API.Actions.generateNewHandshakeNode()
   }
 
   render() {
+    const { displayName, authData, handshakeAddr } = this.state
+
+    if (authData === null) {
+      return <ActivityIndicator size="large" />
+    }
+
     return (
-      <View>
-        <TextInput
-          onChangeText={this.onChange}
-          value={this.state.displayName}
-        />
-        <Button title="Logout" onPress={this.logout} />
-        <Button title="Change" onPress={this.onPress} />
-        <Button title="Continue to Chats" onPress={this.goToChats} />
-        <Button title="Generate Handshake Address" onPress={this.genHandAddr} />
-        <Button title="Clear all cache" onPress={this.clearAllCache} />
+      <View style={styles.container}>
+        <ShockAvatar displayName={displayName} height={180} image={null} />
+
+        {handshakeAddr === null && (
+          <React.Fragment>
+            <Text>
+              No Handshake Address. A handshake address is necessary for being
+              contacted by other users.
+            </Text>
+            <Pad amount={10} />
+
+            <View style={{ maxWidth: '75%' }}>
+              <ShockButton
+                title="Press to Generate a New One"
+                onPress={this.genHandAddr}
+              />
+            </View>
+          </React.Fragment>
+        )}
+
+        {handshakeAddr !== null && (
+          <View style={{ maxWidth: '75%', alignItems: 'center' }}>
+            <Text>Other users can scan this QR to contact you.</Text>
+            <Pad amount={10} />
+            <QR
+              logoToShow="shock"
+              value={`$$__SHOCKWALLET__USER__${
+                authData.publicKey
+              }__${handshakeAddr}__${displayName}`}
+            />
+          </View>
+        )}
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingBottom: 10,
+    paddingTop: 10,
+    width: '100%',
+  },
+  container: {
+    alignItems: 'center',
+    backgroundColor: 'white',
+    flex: 1,
+    justifyContent: 'space-between',
+    paddingBottom: 20,
+  },
+})
