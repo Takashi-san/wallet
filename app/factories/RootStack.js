03d4ea49df420f0d66aaa462d1cce9e5e1033906
Daniel Lugo <danlugo92@gmail.com>
2019-10-14 15:08:54 -0400

    move myprofile  to bottomtab navigator

--- app/factories/RootStack.js
+++ app/factories/RootStack.js
@@ -33,7 +33,7 @@ import Register, { REGISTER } from '../screens/Register'
 import User from '../screens/TheirProfile'
 import Users from '../screens/Users'
 
-import MyProfile from '../screens/MyProfile'
+import MyProfile, { MY_PROFILE } from '../screens/MyProfile'
 import Loading, { LOADING } from '../screens/Loading'
 
 import * as ContactAPI from '../services/contact-api'
@@ -46,9 +46,14 @@ export const MAIN_NAV = 'MAIN_NAV'
 const MainNav = createBottomTabNavigator(
   {
     [WALLET_OVERVIEW]: WalletOverview,
+    [CHATS_ROUTE]: Chats,
+    [MY_PROFILE]: MyProfile,
   },
   {
     initialRouteName: WALLET_OVERVIEW,
+    tabBarOptions: {
+      showLabel: false,
+    },
   },
 )
 
@@ -60,7 +65,6 @@ const App = createStackNavigator(
   {
     [ADVANCED_SCREEN]: Advanced,
     [CHAT_ROUTE]: Chat,
-    [CHATS_ROUTE]: Chats,
     ConnectToHost: ConnectToHost,
     ConfirmPhrase: ConfirmPhrase,
     ConnectToWallet: ConnectToWallet,
@@ -72,7 +76,7 @@ const App = createStackNavigator(
     SearchForUsers: SearchForUsers,
     User,
     Users,
-    MyProfile,
+
     [MAIN_NAV]: MainNav,
   },
   {
@@ -101,7 +105,7 @@ const MainSwitch = createSwitchNavigator(
     [CHOOSE_DISPLAY_NAME]: ChooseDisplayName,
   },
   {
-    initialRouteName: LOADING,
+    initialRouteName: APP,
   },
 )
 
